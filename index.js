040597a78f3d7cdaf03b6f981e58e7c163ace157
Jhair05 <jhair_vkgarcia@outlook.com>
2021-12-13 15:20:33 -0600

    listo

---
+++ index.js
@@ -0,0 +1,17 @@
+var btnCargar = document.getElementById("btnCargar");
+
+btnCargar.addEventListener("click", function () {
+    axios.get("https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8")
+    .then(function (res) {
+        let json = res.data;
+        let lista = document.getElementById("pregunta5");
+        for (var i in json) {
+                let li = document.createElement("li");
+                li.innerHTML = json[i].name + " vive en " + json[i].city + "<br>";
+                lista.appendChild(li);
+        }       
+    })
+    .catch(function (error) {
+        console.log(error)
+    })
+})
